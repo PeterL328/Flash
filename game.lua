2c115c3fd4ef3a94904a52636a94925a32a2d014
Hotcheezy <Peterleng1234@gmail.com>
2016-01-09 00:56:18 -0500

    added some basic laser code

--- game.lua
+++ game.lua
@@ -8,7 +8,14 @@ local json = require( "json" )
 local utility = require( "utility" )
 local myData = require( "mydata" )
 
+
+-- Set up physics engine
+local physics = require("physics")
+physics.start()
+physics.setGravity( 0,0 )
+physics.setDrawMode( "normal" )
 -- 
+
 -- define local variables here
 --
 local currentScore          -- used to hold the numeric value of the current score
@@ -22,8 +29,10 @@ tiles = {}
 level = 5
 buttons = {}
 edges = {} 
-
 mirrorcount = 0
+
+local beamGroup = display.newGroup() -- group for laser objects
+local maxBeams = 50  
 
 --
 -- define local functions here
@@ -137,6 +146,111 @@ function isMirror(gridCount)
 		end
 	end
 end
+
+----------------------
+-- BEGIN laser code
+----------------------
+
+local function clearObject( object )
+    display.remove( object )
+    object = nil
+end
+
+
+local function resetBeams()
+
+    -- Clear all beams/bursts from display
+    for i = beamGroup.numChildren,1,-1 do
+        local child = beamGroup[i]
+        display.remove( child )
+        child = nil
+    end
+
+    -- Reset beam group alpha
+    beamGroup.alpha = 1
+
+end
+
+local function drawBeam( startX, startY, endX, endY )
+
+    -- Draw a series of overlapping lines to represent the beam
+    local beam1 = display.newLine( beamGroup, startX, startY, endX, endY )
+    beam1.strokeWidth = 2 ; beam1:setStrokeColor( 1, 0.312, 0.157, 1 ) ; beam1.blendMode = "add" ; beam1:toBack()
+    local beam2 = display.newLine( beamGroup, startX, startY, endX, endY )
+    beam2.strokeWidth = 4 ; beam2:setStrokeColor( 1, 0.312, 0.157, 0.706 ) ; beam2.blendMode = "add" ; beam2:toBack()
+    local beam3 = display.newLine( beamGroup, startX, startY, endX, endY )
+    beam3.strokeWidth = 6 ; beam3:setStrokeColor( 1, 0.196, 0.157, 0.392 ) ; beam3.blendMode = "add" ; beam3:toBack()
+end
+
+local function castRay( startX, startY, endX, endY )
+
+    -- Perform ray cast
+    local hits = physics.rayCast( startX, startY, endX, endY, "closest" ) 
+    -- Return only the closest hit from the starting point, if any. 
+    -- There is a hit; calculate the entire ray sequence (initial ray and reflections)
+    if ( hits and beamGroup.numChildren <= maxBeams ) then
+
+        -- Store first hit to variable (just the "closest" hit was requested, so use 'hits[1]')
+        local hitFirst = hits[1]
+
+        -- Store the hit X and Y position to local variables
+        local hitX, hitY = hitFirst.position.x, hitFirst.position.y
+
+        -- Place a visual "burst" at the hit point and animate it
+        local burst = display.newImageRect( beamGroup, "burst.png", 64, 64 )
+        burst.x, burst.y = hitX, hitY
+        burst.blendMode = "add"
+        transition.to( burst, { time=1000, rotation=45, alpha=0, transition=easing.outQuad, onComplete=clearObject } )
+
+        -- Draw the next beam
+        drawBeam( startX, startY, hitX, hitY )
+
+        -- Check for and calculate the reflected ray
+        local reflectX, reflectY = physics.reflectRay( startX, startY, hitFirst )
+        local reflectLen = 1600
+        local reflectEndX = ( hitX + ( reflectX * reflectLen ) )
+        local reflectEndY = ( hitY + ( reflectY * reflectLen ) )
+
+        -- If the ray is reflected, cast another ray
+        if ( reflectX and reflectY) then
+            timer.performWithDelay( 40, function() castRay( hitX, hitY, reflectEndX, reflectEndY ); end )
+        end
+
+    -- Else, ray casting sequence is complete
+    else
+
+        -- Draw the final beam
+        drawBeam( startX, startY, endX, endY )
+
+        -- Fade out entire beam group after a short delay
+        transition.to( beamGroup, { time=800, delay=400, alpha=0, onComplete=resetBeams } )
+    end
+end
+
+
+local function fireOnTimer( event )
+
+    -- Ensure that all previous beams/bursts are cleared/complete before firing
+    if beamGroup.numChildren == 0 then
+
+        -- Stop rotating turret as it fires
+        turret.angularVelocity = 0
+
+        -- Play laser sound
+        audio.play( sndLaserHandle )
+
+        -- Calculate ending x/y of beam
+        local xDest = turret.x - (math.cos(math.rad(turret.rotation+90)) * 1600 )
+        local yDest = turret.y - (math.sin(math.rad(turret.rotation+90)) * 1600 )
+
+        -- Cast the initial ray
+        castRay( turret.x, turret.y, xDest, yDest )
+    end
+end
+
+----------------------
+-- END laser code
+----------------------
 
 function scene:create( event )
     --
